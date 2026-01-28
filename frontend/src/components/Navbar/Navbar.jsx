3e335e927dd3519134a8db38cb26468b16b5f70b
Vishwash-chauhan <vishwashchauhan6231@gmail.com>
2026-01-28 12:29:47 +0530

    nav bar update

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -9,8 +9,8 @@ const navLinks = [
   // { path: '/services', label: 'Services' },
   // { path: '/portfolio', label: 'Portfolio' },
   // { path: '/blog', label: 'Blog' },
-  { path: '/video-marketing', label: 'Video Marketing' },
   { path: '/digital-marketing', label: 'Digital Marketing' },
+  { path: '/video-marketing', label: 'Video Marketing' },
   { path: '/contact', label: 'Contact' },
 ];
 
